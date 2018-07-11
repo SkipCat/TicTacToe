4db49020f0657a57b3233ea0742d4b4c691f64c8
SkipCat <cecilehuguet@laposte.net>
2018-07-11 16:27:51 +0200

    Added button click interaction

--- tictactoe/OfflineController.swift
+++ tictactoe/OfflineController.swift
@@ -31,6 +31,7 @@ class OfflineController: UIViewController {
     @IBAction func restartAction(_ sender: UIButton) {
         self.values = [0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.playerTurn = 1
+        // TODO: set buttons invisible by default and then display x / o image
         bt0.setTitle("Button", for: .normal)
         bt1.setTitle("Button", for: .normal)
         bt2.setTitle("Button", for: .normal)
@@ -48,8 +49,8 @@ class OfflineController: UIViewController {
     }
     
     @IBAction func handleClick(_ sender: UIButton) {
-        
         sender.setTitle(String(self.playerTurn), for: .normal)
+        sender.isEnabled = false
         self.values[sender.tag] = self.playerTurn
         
         if (
@@ -63,7 +64,6 @@ class OfflineController: UIViewController {
                 (self.values[2] != 0 && self.values[2] == self.values[4] && self.values[4] == self.values[6])
             ) {
             winnerText.text = "\(self.playerTurn) won the game"
-            
             
             if var opened:Array<Int> = defaults.array(forKey:"winHistory") as! Array<Int>?{
                 opened.append(self.playerTurn)
--- tictactoe/OnlineController.swift
+++ tictactoe/OnlineController.swift
@@ -4,9 +4,9 @@ import SocketIO
 class OnlineController: UIViewController {
     
     @IBOutlet weak var GridImage: UIImageView!
-    @IBOutlet weak var CurrentTurn: UITextField!
-    @IBOutlet weak var PlayerNameX: UITextField!
-    @IBOutlet weak var PlayerNameO: UITextField!
+    @IBOutlet weak var CurrentTurn: UILabel!
+    @IBOutlet weak var PlayerNameX: UILabel!
+    @IBOutlet weak var PlayerNameO: UILabel!
     
     var playersData: NSArray = []
     var playerX: String?
@@ -17,6 +17,7 @@ class OnlineController: UIViewController {
         super.viewDidLoad()
         
         let castData = playersData[0] as! [String: Any]
+        print(castData)
         self.playerTurn = (castData["currentTurn"] as? String)?.uppercased()
         
         self.playerX = castData["playerX"] as? String
@@ -25,14 +26,39 @@ class OnlineController: UIViewController {
         self.PlayerNameO.text = "Player O: \(self.playerO!)"
         
         if (self.playerTurn == "O") {
-            self.CurrentTurn.text = "Player \(self.playerO!) turn"
+            self.CurrentTurn.text = "It is \(self.playerO!) turn"
         } else {
-            self.CurrentTurn.text = "Player \(self.playerX!) turn"
+            self.CurrentTurn.text = "It is \(self.playerX!) turn"
+        }
+        
+        TTTSocket.sharedInstance.socket.on("movement") { data, _ in
+            let castData = data[0] as! [String: Any]
+            let playerPlayed = (castData["player_played"] as? String)?.uppercased()
+            
+            if (castData["err"] == nil) {
+                self.CurrentTurn.text = "Wrong move, please try again"
+            } else if (playerPlayed != self.playerTurn) {
+                self.CurrentTurn.text = "It is not your turn"
+            } else {
+                let buttonClicked = self.view.viewWithTag((castData["index"] as? Int)!) as? UIButton
+                buttonClicked?.setTitle(playerPlayed!, for: .normal)
+                buttonClicked?.isEnabled = false
+                
+                if ((castData["win"] as? Int) == 1) {
+                    self.CurrentTurn.text = "Player \(playerPlayed!) won!"
+                } else {
+                    self.playerTurn = (self.playerTurn == "X" ? "O" : "X")
+                }
+            }
         }
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+    
+    @IBAction func handleClick(_ sender: UIButton) {
+        TTTSocket.sharedInstance.socket.emit("movement", sender.tag)
     }
     
     @IBAction func dismiss(_ sender: Any) {
